75765ddb379a387b01f5814bbbc55aca542e4a84
Изтлеу Дархан <darkhna.iztleu@bcchub.kz>
2025-06-13 17:54:36 +0500

    init

---
+++ Isharp/Models/StandardArchive.cs
@@ -0,0 +1,9 @@
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace Isharp.Models;
+
+[PageType(Title = "Standard archive", IsArchive = true)]
+public class StandardArchive : Page<StandardArchive>
+{
+}
---
+++ Isharp/Models/StandardPost.cs
@@ -0,0 +1,9 @@
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace Isharp.Models;
+
+[PostType(Title = "Standard post")]
+public class StandardPost : Post<StandardPost>
+{
+}
